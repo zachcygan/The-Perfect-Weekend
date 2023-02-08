d213e14ddcdb9ce7fb40d1f8584abfc89cae9dd0
Zachary Cygan <zachcygan@gmail.com>
2023-02-07 20:00:54 -0800

    added background gradient color

--- assets/JS/resultsPagejs.js
+++ assets/JS/resultsPagejs.js
@@ -19,58 +19,62 @@ fetch(url, {
         var locationLon = data.businesses[0].coordinates.longitude;
 
         for(var i = 0; i < data.businesses.length; i++) {
-            var resultCard = $('<div>');
-            var resultTitle = $('<p>');
-            var titleContainer = $('<div>');
-            var imgContainer = $('<div>');
-            var bodyContainer = $('<div>');
-            var resultImg = $('<img>');
-            var imgFigure = $('<figure>');
-            var phoneNumber = $('<div>');
-            var businessRating = $('<div>');
-            var mediaContainer = $('<div>');
-            var businessPrice = $('<div>');
-            var contentContainer = $('<div>');
-            var businessReviews = $('<div>');
-            var cardImg = data.businesses[i].image_url;
-            resultImg.attr('src', cardImg);
-            
-            $foodAndDrinkRec.addClass(['custom-flex'])
-            resultCard.addClass(['card', 'column', 'is-one-fifth', 'm-1', 'custom-card']);
-            resultImg.addClass(['image']);
-            imgFigure.addClass(['image', 'is-4by3'])
-            imgContainer.addClass('card-image');
-            bodyContainer.addClass('card-content');
-            titleContainer.addClass(['media-content']);
-            titleContainer.css('min-height', '30%')
-            mediaContainer.addClass(['media']);
-            resultTitle.addClass(['title', 'is-4'])
-            contentContainer.addClass('content');
-            
-            phoneNumber.text('Phone: ' + data.businesses[i].display_phone);
-            businessRating.text('Rating: ' + data.businesses[i].rating + '⭐')
-
-            if (data.businesses[i].price === undefined) {
-                businessPrice.text('Price: N/A')
-            } else {
-                businessPrice.text('Price: ' + data.businesses[i].price)
-            }
-            businessReviews.text('Number of reviews: ' + data.businesses[i].review_count)
-            resultTitle.text(data.businesses[i].name);
-    
-            imgContainer.append(imgFigure);
-            imgFigure.append(resultImg);
-            bodyContainer.append(mediaContainer);
-            mediaContainer.append(titleContainer);
-            bodyContainer.append(contentContainer);
-            contentContainer.append(phoneNumber);
-            contentContainer.append(businessRating);
-            contentContainer.append(businessReviews);
-            contentContainer.append(businessPrice);
-            titleContainer.append(resultTitle);
-            resultCard.append(imgContainer);
-            resultCard.append(bodyContainer);
-            $foodAndDrinkRec.append(resultCard);
+            fetchSearchResults(data.businesses[i])
         }
     })
 
+function fetchSearchResults(data) {
+    
+        var resultCard = $('<div>');
+        var resultTitle = $('<p>');
+        var titleContainer = $('<div>');
+        var imgContainer = $('<div>');
+        var bodyContainer = $('<div>');
+        var resultImg = $('<img>');
+        var imgFigure = $('<figure>');
+        var phoneNumber = $('<div>');
+        var businessRating = $('<div>');
+        var mediaContainer = $('<div>');
+        var businessPrice = $('<div>');
+        var contentContainer = $('<div>');
+        var businessReviews = $('<div>');
+        var cardImg = data.image_url;
+        resultImg.attr('src', cardImg);
+        
+        $foodAndDrinkRec.addClass(['custom-flex'])
+        resultCard.addClass(['card', 'column', 'is-one-fifth', 'm-1', 'custom-card']);
+        resultImg.addClass(['image']);
+        imgFigure.addClass(['image', 'is-4by3'])
+        imgContainer.addClass('card-image');
+        bodyContainer.addClass('card-content');
+        titleContainer.addClass(['media-content']);
+        titleContainer.css('min-height', '30%')
+        mediaContainer.addClass(['media']);
+        resultTitle.addClass(['title', 'is-4'])
+        contentContainer.addClass('content');
+        
+        phoneNumber.text('Phone: ' + data.display_phone);
+        businessRating.text('Rating: ' + data.rating + '⭐')
+
+        if (data.price === undefined) {
+            businessPrice.text('Price: N/A')
+        } else {
+            businessPrice.text('Price: ' + data.price)
+        }
+        businessReviews.text('Number of reviews: ' + data.review_count)
+        resultTitle.text(data.name);
+
+        imgContainer.append(imgFigure);
+        imgFigure.append(resultImg);
+        bodyContainer.append(mediaContainer);
+        mediaContainer.append(titleContainer);
+        bodyContainer.append(contentContainer);
+        contentContainer.append(phoneNumber);
+        contentContainer.append(businessRating);
+        contentContainer.append(businessReviews);
+        contentContainer.append(businessPrice);
+        titleContainer.append(resultTitle);
+        resultCard.append(imgContainer);
+        resultCard.append(bodyContainer);
+        $foodAndDrinkRec.append(resultCard);
+    }
