37694c6fd970caffb461aed8683d0ff587d777b5
Zachary Cygan <zachcygan@gmail.com>
2023-02-07 19:09:53 -0800

    updated code for cleaner look

--- assets/JS/resultsPagejs.js
+++ assets/JS/resultsPagejs.js
@@ -1,5 +1,4 @@
 var url = 'https://afternoon-badlands-11870.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=Irvine&term=sushi&sort_by=best_match&limit=20'
-var url2 = 'https://afternoon-badlands-11870.herokuapp.com/https://api.yelp.com/v3/businesses/P76bdceIwmVe4F7OVhfTYA/reviews'
 var $foodAndDrinkRec = $('#foodAndDrinkRec');
 
 
@@ -67,21 +66,4 @@ fetch(url, {
             resultCard.append(bodyContainer);
             $foodAndDrinkRec.append(resultCard);
         }
-
-        
-        
     })
-
-    fetch(url2, {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer 4HSUlXQrk6K2CdfXtepX9Kd9bTmVhrT7OOi_0m7xJzj92B7XSuHTEwp93qkzz2LZ0PfvapAxEQnB3E6NsThaOAgtJP-myli-rvN0M-a9vhmpwldwJPIJ7rA9aCLgY3Yx'
-        }
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-    
-            
-        })
